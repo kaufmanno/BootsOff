5fe1566f7cf8640427b4dfc9811de46bac4ebf4d
su530201 <olivier.kaufmann@umons.ac.be>
2021-04-25 21:27:45 +0200

    Build docs

--- docs/source/definitions.py
+++ docs/source/definitions.py
@@ -6,5 +6,5 @@ VERSION = u'0.2'
 RELEASE = VERSION + u'.1a'
 LICENSE = u'GPLv3'
 SHORT_DESCRIPTION = ''
-ROOT_DIR = Path(__file__).parent.absolute()
+ROOT_DIR = str(Path(__file__).parent.absolute())
 URL = f'https://github.com/kaufmanno/{PROJECT}'
