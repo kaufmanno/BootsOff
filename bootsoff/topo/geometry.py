ef9b87bd6496edef642efe0cea0f80bb907c2142
su530201 <olivier.kaufmann@umons.ac.be>
2021-10-03 14:22:59 +0200

    Add plane_normal function in geometry

--- bootsoff/topo/geometry.py
+++ bootsoff/topo/geometry.py
@@ -85,6 +85,39 @@ def lift(obj, elevation=0.):
     # return obj  # not needed as operation is done inplace
 
 
+def plane_normal(points):
+    """Returns the normal vector of a plane defined by three non co-linear 3D points
+
+    Parameters
+    ----------
+    points : tuple or list or numpy.array
+        an array-like object of the coordinates of three 3D points
+
+    Returns
+    -------
+    numpy.array
+        the vector normal to the plane
+
+    Examples
+    --------
+    This example shows how to search for the normal vector of the Oxy plane (assuming first second and thrid components
+    of the vectors correspond to components along x, y and z respectively in a cartesian system :
+
+    >>> import numpy as np
+    >>> a = np.array((0., 0., 0.))
+    >>> b = np.array((1., 0., 0.))
+    >>> c = np.array((0., 1., 0.))
+    >>> plane_normal([a, b, c])
+    array([0., 0., 1.])
+
+    The answer is obviously a unit vector along the z-axis.
+    """
+    a, b, c = points
+    n = np.cross(b-a, c-a)
+    n = n/np.linalg.norm(n)
+    return n
+
+
 def rotate_around_vector(vector, angle, degrees=False, affine_4x4=False):
     """Returns the matrix associated to a rotation by a given angle around a given vector using Rodrigues formula
 
@@ -118,7 +151,7 @@ def rotate_around_vector(vector, angle, degrees=False, affine_4x4=False):
     array([ 0.00000000e+00, -1.00000000e+00,  1.11022302e-16])
 
     For more info. see https://en.wikipedia.org/wiki/Rodrigues'_rotation_formula#Matrix_notation"""
-    
+
     vector = np.array(vector)  # convert array-like (list, tuple) into a numpy array
     vector = vector/np.linalg.norm(vector) # normalize the vector
     if degrees:
