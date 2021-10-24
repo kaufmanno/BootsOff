c36e2445b044206a60ed05855695fb8cd45d5c8d
su530201 <olivier.kaufmann@umons.ac.be>
2021-10-24 18:30:20 +0200

    Improves defintions.py with version and release from git tags

--- definitions.py
+++ definitions.py
@@ -1,10 +1,20 @@
 from pathlib import Path
 
+def get_version():
+    cmd = ['git', 'describe', '--tags', '--abbrev=0']
+    output = check_output(cmd)
+    return output.decode('latin1').strip()
+    
+def get_release():
+    cmd = ['git', 'describe', '--tags']
+    output = check_output(cmd)
+    return output.decode('latin1').strip()
+
 PROJECT = u'BootsOff'
 AUTHOR = u'O. KAUFMANN'
-VERSION = u'0.2'
-RELEASE = VERSION + u'.1a'
+VERSION = get_version()
+RELEASE = get_release()
 LICENSE = u'GPLv3'
-SHORT_DESCRIPTION = ''
+SHORT_DESCRIPTION = 'Bootsoff bundles tools for applied geophysics'
 ROOT_DIR = str(Path(__file__).parent.absolute())
 URL = f'https://github.com/kaufmanno/{PROJECT}'
